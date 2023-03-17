23d9090d699f15d876dec8df436eb8a9f0f08d94
Sephadeus <90430093+Sephadeus@users.noreply.github.com>
2023-03-17 13:34:39 -0400

    Updates

---
+++ precache-manifest.6242307371765c5a88fe158e6644b652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b36398bad43301080df3",
+    "url": "/JETSETTR/static/css/main.5fc13198.chunk.css"
+  },
+  {
+    "revision": "b36398bad43301080df3",
+    "url": "/JETSETTR/static/js/main.af1d646b.chunk.js"
+  },
+  {
+    "revision": "d156e395a785be990080",
+    "url": "/JETSETTR/static/js/runtime~main.344be2b8.js"
+  },
+  {
+    "revision": "68bc17032788ccc94b95",
+    "url": "/JETSETTR/static/js/2.8bbc7f52.chunk.js"
+  },
+  {
+    "revision": "516b49cbee20fd86c3d5e8dc782bf2db",
+    "url": "/JETSETTR/static/media/Jetsettr.516b49cb.png"
+  },
+  {
+    "revision": "876818c23b159be512ca5171dbb33bce",
+    "url": "/JETSETTR/index.html"
+  }
+];
